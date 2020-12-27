3b5122583bbdad016d934f461e3503a3fb6d69e9
carterghill <carterghill@gmail.com>
2020-12-27 17:38:55 -0600

    Created Layout.js to wrap the header and footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,13 @@
-
+import Footer from './Footer'
 import React, {Component} from 'react'
 
 class Layout extends Component {
     render() {
         return (
-            <div>
-                <div> Header </div>
-                {this.props.children}
-                <div> Footer </div>
+            <div className='layout'>
+                <div className='header'> Header </div>
+                <div className='content'>{this.props.children}</div>
+                <div className='footer'> Footer </div>
             </div>
         )
     }
