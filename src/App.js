288a8cf63d3eb2f812fb6f05016c61c7d060919f
carterghill <carterghill@gmail.com>
2020-12-27 22:57:46 -0600

    Adding routing to pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import Layout from './components/Layout'
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import About from "./pages/About"
+import Resources from "./pages/Resources"
+import Contact from "./pages/Contact"
 import './App.css';
+
+const PageNotFound = () => (
+    <div>
+        <h1>Error - 404</h1>
+        <p>The page you've tried to acces does not seem to exist</p>
+    </div>
+)
 
 function App() {
 
@@ -10,8 +22,29 @@ function App() {
     <BrowserRouter>
     <Layout>
 
-        Hello world!!!
+        <div className='page'>
+            <Route render={({location}) => (
+                <TransitionGroup>
+                    <CSSTransition
+                        key={location.key}
+                        timeout={750}
+                        classNames='fade-route'
+                    >
+                    <Switch location={location}>
+                        <Route path="/" component={Home} exact={true}/>
+                        <Route path="/home" component={Home} exact={true}/>
+                        <Route path="/projects" component={Projects} exact={true}/>
+                        <Route path="/about" component={About} exact={true}/>
+                        <Route path="/resources" component={Resources} exact={true}/>
+                        <Route path="/contact" component={Contact} />
+                        <Route component={PageNotFound} exact={true}/>
+                    </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+            )} />
 
+        </div>
+        
     </Layout>
     </BrowserRouter>
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class About extends Component {
+    render() {
+        return (
+        <div>
+
+            <div className='header'>Web Design, Full Stack, and Custom App Developement</div>
+
+        </div>
+        )
+    }
+}
+
+export default About
