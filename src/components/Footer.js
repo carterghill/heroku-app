d28cf7bcc15973341a112d523e4547936b59382a
carterghill <carterghill@gmail.com>
2020-12-30 23:11:11 -0600

    Added links and css to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import './Footer.css'
 
 class Footer extends Component {
     render() {
         return (
             <div className='footer'>
-				FOOTER
+				<Link className='footerLink' to='/home'>Home</Link>
+                •
+                <Link className='footerLink' to='/about'>About</Link>
+                •
+                <Link className='footerLink' to='/projects'>Projects</Link>
+                •
+                <Link className='footerLink' to='/resources'>Resources</Link>
+                •
+                <Link className='footerLink' to='/contact'>Contact</Link>
             </div>
         )
     }
